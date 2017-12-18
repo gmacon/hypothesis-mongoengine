286af69069c60723048161cdeb14888a7e29a7bd
George Macon <george.macon@gmail.com>
2017-12-18 11:28:20 -0500

    Flesh out setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,35 @@
+import io
+import os.path
+
 from setuptools import find_packages, setup
+
+
+with io.open(os.path.join(os.path.dirname(__file__), 'README.rst'),
+             encoding='utf-8') as f:
+    long_description = f.read()
 
 
 setup(
     name='hypothesis-mongoengine',
     version='0.1.0',
     description='Hypothesis strategy for MongoEngine models',
+    long_description=long_description,
+    url='https://github.com/gmacon/hypothesis_mongoengine',
+    author='George Macon <george.macon@gmail.com>',
+    license='MIT',
     packages=find_packages(exclude='*.tests'),
     install_requires=[
         'hypothesis',
         'mongoengine',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Testing',
     ],
 )
